a7272d831a1120a474429dc6db128b137cfbc2c2
Angel Sullon <asullom@gmail.com>
2018-05-03 18:22:49 -0500

    first commit

--- P03.java
+++ P03.java
@@ -5,19 +5,22 @@ public class P03 {
 		Scanner reader = new Scanner(System.in);
 		System.out.print("n:");
 		int n = Integer.parseInt( reader.nextLine() );
+		if (primo(n)) {
+			System.out.println(n+" es primo");
+		}else{
+			System.out.println(n+" NO es primo");
+		}
+	}
+	public static boolean primo(int n) {
 		int i=2;
 		boolean esPrimo= true;
-		while(i < n ) {
+		while(i < n && esPrimo) {
 			if (n % i == 0 ) {				
 				System.out.println(i+" ");
 				esPrimo = false;
 			}
 			i++;
 		}
-		if (esPrimo) {
-			System.out.println(n+" es primo");
-		}else{
-			System.out.println(n+" NO es primo");
-		}
+		return esPrimo;
 	}
 } 
